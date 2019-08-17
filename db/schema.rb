656f543f4d797af0031d6a012943c1653f60d842
Tracy Ngo <tngo1206@gmail.com>
2019-08-16 17:13:49 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,8 @@ ActiveRecord::Schema.define(version: 2019_08_16_201539) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
     t.string "password_digest"
     t.string "title"
