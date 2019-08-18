f27808f41eacc0bf2aa3ea7390e9361f30f264f7
Tracy Ngo <tngo1206@gmail.com>
2019-08-18 15:30:46 -0700

    update model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :logs
-  has_many :workouts, through: :log_workouts
 
   validates :email, uniqueness: true
   validates :email, presence: true
