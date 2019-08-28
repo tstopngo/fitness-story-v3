72b0d3eb52d368791aa96d6d5870d9b23522ff2c
Tracy Ngo <tngo1206@gmail.com>
2019-08-28 13:21:54 -0700

    working amount field

--- app/models/log.rb
+++ app/models/log.rb
@@ -23,33 +23,43 @@ class Log < ApplicationRecord
 
 
   #when creating a new workout from form check to see if it is db first
-  #def workouts_attributes=(workouts_attributes)
-  #  workouts_attributes.values.each do |workout_attribute|
-  #    workout = Workout.find_or_create_by(wpr)
-  #    self.workouts << workout unless self.workouts.include?(workout)
-  #    end
-  #  end
+  		def workouts_attributes=(workouts_attributes)
 
-    def workouts_attributes=(workouts_attributes)
-      workouts_attributes.values.each do |workout_attribute|
-        workout= Workout.find_or_create_by(name: workout_attribute[:name], workout_type: workout_attribute[:workout_type])
-        self.workouts << workout unless self.workouts.include?(workout)
-        workout_attribute[:log_workouts_attributes].values.each do |log_workout_attribute|
+        workouts_attributes.values.each do |workout_attribute
+        #workous_attributes gives you the values of the hash, which has an array of "workout objects"
 
-        if self.log_workouts.any? do |log_workout|
-            log_workout.log_id == self.id && log_workout.workout_id == workout.id
+          workout= Workout.find_or_create_by(name: workout_attribute[:name], workout_type: workout_attribute[:workout_type])
+          #find the workout object from the pipes or create a new one with the arguments
+
+          self.workouts << workout unless self.workouts.include?(workout)
+          #add the workout object to the proxy collection unless it has it already
+
+          workout_attribute[:log_workouts_attributes].values.each do |log_workout_attribute|
+          #key into the nested information workout attribute and grab the log_workout attributes, which includes amount, wokrout_id, and log_id
+
+  				if self.log_workouts.any? do |log_workout|
+            #check if logs have any log_workouts if true then pass each log_workout through pipes
+
+              log_workout.log_id == self.id && log_workout.workout_id == workout.id
+              #set the the ids of the log and workout to the log_workout
+            end
+            log_workout = self.log_workouts.select do |i|
+  					#select the log_workout from logs
+              i.workout_id == workout.id && i.log_id == self.id
+  						#select the log_workout from the log_wokrouts where the the Workout ID and Log Id match
+            end.first
+
+            log_workout.amount = log_workout_attribute[:amount] unless log_workout_attribute.blank?
+  					#write the amount to the Join Table
+            log_workout.save
+
+
+          else
+            log_workout = self.log_workouts.select {|i| i.workout_id == workout.id}.first
+            log_workout.amount = log_workout_attribute unless log_workout_attribute.blank?
+  					#if there are no log workouts then then select the workout id that matches and write the amount
+            log_workout.save
           end
-          log_workout = self.log_workouts.select do |i|
-            i.workout_id == workout.id && i.log_id == self.id
-          end.first
-          log_workout.amount = log_workout_attribute[:amount] unless log_workout_attribute.blank?
-          log_workout.save
-        else
-          log_workout = self.log_workouts.select {|i| i.workout_id == workout.id}.first
-          log_workout.amount = log_workout_attribute unless log_workout_attribute.blank?
-          log_workout.save
         end
-      end
-      end
-    end
+        end
   end
